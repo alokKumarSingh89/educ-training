dbb7c8d2bd8b70947219594cc217b22d1783d3ee
code with Kamlesh <kamleshrajput5394@gmail.com>
2022-02-27 21:53:38 +0530

    Leaning tailwind

--- src/Navbar.js
+++ src/Navbar.js
@@ -4,7 +4,7 @@ export const Navbar = () => {
   return (
     <nav className='px-4 py-4 bg-purple-900 text-white'>
         <ul className='flex'>
-            <li className='mx-2 cursor-pointer'>Home</li>
+            <li className='mx-2 cursor-pointer fle'>Home</li>
             <li className='mx-2 cursor-pointer'>About</li>
             <li className='mx-2 cursor-pointer'>Contact</li>
         </ul>
